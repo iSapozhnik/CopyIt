9b1505b4ffe126de99f64b647acf59671b8f2301
Ivan Sapozhnik <ivan.sapozhnik@sixt.com>
2018-11-16 22:09:30 +0100

    Support of different actions

--- CopyIt/Classes/CopyIt.swift
+++ CopyIt/Classes/CopyIt.swift
@@ -13,7 +13,7 @@ private class GestureRecognizer {
     private var handler: ((UIGestureRecognizer) -> Void)?
     var recognizer: UIGestureRecognizer?
     
-    public func observe(with handler: ((UIGestureRecognizer) -> Void)?) {
+    public func onLongPress(_ handler: ((UIGestureRecognizer) -> Void)?) {
         recognizer = UILongPressGestureRecognizer(
             target: self,
             action: #selector(handlePress(_:)))
@@ -21,30 +21,66 @@ private class GestureRecognizer {
     }
     
     @objc func handlePress(_ gesture: UIGestureRecognizer) {
-        guard gesture.state != .began else { return }
-        handler?(gesture)
+        if gesture.state == .began {
+            handler?(gesture)
+        }
+    }
+}
+
+private enum Actions: Selector {
+    case copyText
+    case copyImage
+    case copyView
+
+    var action: Selector {
+        switch self {
+        case .copyText:
+            return #selector(ActionHolderView.copyTextAction(_:))
+        case .copyImage:
+            return #selector(ActionHolderView.copyImageAction(_:))
+        case .copyView:
+            return #selector(ActionHolderView.copyViewAction(_:))
+        }
+    }
+
+    static var all: [Selector] {
+        return [Actions.copyText.action, Actions.copyImage.action, Actions.copyView.action]
     }
 }
 
 private class ActionHolderView: UIView {
-    private var action: () -> Void
-    
-    init(action: @escaping () -> Void) {
-        self.action = action
+    typealias Action = () -> Void
+    private var copyTextAction: Action?
+    private var copyImageAction: Action?
+    private var copyViewAction: Action?
+
+    init(copyTextAction: Action?, copyImageAction: Action?, copyViewAction: Action?) {
+        self.copyTextAction = copyTextAction
+        self.copyImageAction = copyImageAction
+        self.copyViewAction = copyViewAction
         super.init(frame: .zero)
-    }
-
-    deinit {
-
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    @objc func copyAction(_ controller: UIMenuController) {
-        action()
+    @objc func copyTextAction(_ controller: UIMenuController) {
+        copyTextAction?()
         resignFirstResponder()
+        removeFromSuperview()
+    }
+
+    @objc func copyImageAction(_ controller: UIMenuController) {
+        copyImageAction?()
+        resignFirstResponder()
+        removeFromSuperview()
+    }
+
+    @objc func copyViewAction(_ controller: UIMenuController) {
+        copyViewAction?()
+        resignFirstResponder()
+        removeFromSuperview()
     }
     
     override open var canBecomeFirstResponder: Bool {
@@ -53,25 +89,34 @@ private class ActionHolderView: UIView {
     }
     
     override open func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
-        return action == #selector(copyAction(_:))
-    }
-}
-
-private class CopyTextMenuItem: UIMenuItem {
-    var actionHolderView: ActionHolderView
-    
-    init(title: String, block: @escaping () -> Void) {
-        self.actionHolderView = ActionHolderView(action: block)
-        super.init(title: title, action: #selector(ActionHolderView.copyAction(_:)))
-    }
-
-    deinit {
-        actionHolderView.removeFromSuperview()
+        return Actions.all.contains(action)
     }
 }
 
 private struct AssociatedKeys {
     static var gesture: UInt8 = 0
+}
+
+public enum CopiableOptions {
+    case text
+    case image
+    case view
+
+    public static var all: [CopiableOptions] {
+        return [.text, .image, .view]
+    }
+}
+
+public struct CopiableLocalization {
+    let copyText: String
+    let copyImage: String
+    let copyView: String
+
+    public init(copyText: String = "Copy text", copyImage: String = "Copy image", copyView: String = "Copy view") {
+        self.copyText = copyText
+        self.copyImage = copyImage
+        self.copyView = copyView
+    }
 }
 
 public extension Copiable where Self: UIView {
@@ -86,32 +131,79 @@ public extension Copiable where Self: UIView {
             objc_setAssociatedObject(self, &AssociatedKeys.gesture, newValue, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
         }
     }
-    
-    public mutating func enableCopying(with content: @escaping (Self?) -> String) {
+
+    public mutating func enableCopying<T>(_ content: ((Self?) -> T)?) {
+        enableCopying(options: CopiableOptions.all, localization: CopiableLocalization(), content)
+    }
+
+    public mutating func enableCopying<T>(localization: CopiableLocalization, _ content: ((Self?) -> T)?) {
+        enableCopying(options: CopiableOptions.all, localization: localization, content)
+    }
+
+    public mutating func enableCopying<T>(options: [CopiableOptions], localization: CopiableLocalization, _ content: ((Self?) -> T)?) {
         isUserInteractionEnabled = true
         
         gesture = GestureRecognizer()
-        gesture?.observe(with: { [weak self] gesture in
-            print("Observer executed")
-            guard let gestureView = gesture.view, let superView = gestureView.superview else {
-                return
+        gesture?.onLongPress { [weak self] gesture in
+            guard let gestureView = gesture.view, let superView = gestureView.superview else { return }
+
+            var filteredOptions: [CopiableOptions] = options
+            if self?.isMember(of: UILabel.self) == true || (self?.isKind(of: UIView.self) == true && self?.isMember(of: UIImageView.self) == false)     {
+                filteredOptions.removeAll(where: { $0 == CopiableOptions.image })
             }
+            if self?.isMember(of: UIImageView.self) == true {
+                filteredOptions.removeAll(where: { $0 == CopiableOptions.text || $0 == CopiableOptions.view })
+            }
+
+            guard filteredOptions.count > 0 else { return }
             
             let menuController = UIMenuController.shared
-            
-            let copyItem = CopyTextMenuItem(title: "COPY ME", block: {
-                UIPasteboard.general.string = content(self)
+
+            let release = {
                 menuController.menuItems = nil
-            })
-            self?.addSubview(copyItem.actionHolderView)
-            
-            guard !menuController.isMenuVisible, copyItem.actionHolderView.canBecomeFirstResponder else { return }
-            copyItem.actionHolderView.becomeFirstResponder()
-            menuController.menuItems = [copyItem]
+            }
+
+            let copyText = {
+                guard let content = content, let contentValue = content(self) as? String else { return }
+                UIPasteboard.general.string = contentValue
+                release()
+            }
+
+            let copyImage = {
+                guard let content = content, let contentValue = content(self) as? UIImage else { return }
+                UIPasteboard.general.image = contentValue
+                release()
+            }
+
+            let copyView = {
+                UIPasteboard.general.image = self?.asImage()
+                release()
+            }
+
+            let actionHolderView = ActionHolderView(copyTextAction: copyText, copyImageAction: copyImage, copyViewAction: copyView)
+
+            var items = [UIMenuItem]()
+            filteredOptions.forEach { option in
+                switch option {
+                case .text:
+                    items.append(UIMenuItem(title: "Copy text", action: Actions.copyText.action))
+                case .image:
+                    items.append(UIMenuItem(title: "Copy image", action: Actions.copyImage.action))
+                case .view:
+                    items.append(UIMenuItem(title: "Copy view", action: Actions.copyView.action))
+                }
+            }
+
+            self?.addSubview(actionHolderView)
+
+            guard !menuController.isMenuVisible, actionHolderView.canBecomeFirstResponder else { return }
+            actionHolderView.becomeFirstResponder()
+            menuController.menuItems = items
             
             menuController.setTargetRect(gestureView.frame, in: superView)
+//            menuController.arrowDirection = self?.arrowDirection(for: gestureView) ?? .down
             menuController.setMenuVisible(true, animated: true)
-        })
+        }
         
         guard let recognizer = gesture?.recognizer else { return }
 
@@ -119,5 +211,33 @@ public extension Copiable where Self: UIView {
             self.addGestureRecognizer(recognizer)
         }
     }
+
+//    private func arrowDirection(for view: UIView) -> UIMenuControllerArrowDirection {
+//        let window = UIApplication.shared.keyWindow?.rootViewController?.view
+//        let width = window?.frame.width
+//        let height = window?.frame.height
+//        let frame = view.superview?.convert(view.frame, to: window)
+//
+//        if height! - frame!.minY <= 10 {
+//            return UIMenuControllerArrowDirection.up
+//        }
+//        return .up
+//    }
 }
 
+extension UIView {
+    func asImage() -> UIImage {
+        if #available(iOS 10.0, *) {
+            let renderer = UIGraphicsImageRenderer(bounds: bounds)
+            return renderer.image { rendererContext in
+                layer.render(in: rendererContext.cgContext)
+            }
+        } else {
+            UIGraphicsBeginImageContext(self.frame.size)
+            self.layer.render(in:UIGraphicsGetCurrentContext()!)
+            let image = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            return UIImage(cgImage: image!.cgImage!)
+        }
+    }
+}
--- Example/CopyIt/ViewController.swift
+++ Example/CopyIt/ViewController.swift
@@ -9,22 +9,31 @@
 import UIKit
 import CopyIt
 
-class MyView: UIView, Copiable {}
-extension UILabel: Copiable {}
+class MyView: UIView {}
+extension UIView: Copiable {}
 
 class ViewController: UIViewController {
     @IBOutlet weak var someView: MyView!
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        someView.enableCopying { (view) -> String in
-            return "Hello world!"
+
+        someView.enableCopying { view -> UIView? in
+            return view
         }
         
-        label.enableCopying { (label) -> String in
-            return label?.text ?? ""
+        label.enableCopying { label -> String? in
+            return label?.text
+        }
+
+        imageView.enableCopying { imageView -> UIImage? in
+            return imageView?.image
+        }
+
+        view.enableCopying { [weak self] _ in
+            return self?.view
         }
     }
 }
